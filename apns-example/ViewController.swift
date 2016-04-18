f872894223c37a42681652b68dbc944a47edf3a4
Jakub Machon <jakub.machon@chimeraprime.com>
2016-04-18 12:21:32 +0200

    Initial Commit

---
+++ apns-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  apns-example
+//
+//  Created by Jakub Machoń on 18.04.2016.
+//  Copyright © 2016 Syncano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
